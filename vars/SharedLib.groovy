62aa06e5243195182f67333657f4c7048559474f
cdc007 <dacheng_chen@yahoo.com>
2021-04-17 21:34:12 -0700

    Update SharedLib.groovy

---
+++ vars/SharedLib.groovy
@@ -0,0 +1,13 @@
+
+//@groovy.transform.Field def workspace
+//   @groovy.transform.Field def path
+//  @groovy.transform.Field def file
+
+@Library('commonLib@master') _
+
+timestamps {
+
+       SharedLib{
+           url:'https://api.beta.shipwire.com/exec/InventoryServices.ph'
+       }
+}
