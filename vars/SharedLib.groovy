c2b74e20befaf0ad514077c245c8fd2a420c9b36
cdc007 <dacheng_chen@yahoo.com>
2021-04-18 18:39:24 -0700

    Update SharedLib.groovy

--- vars/SharedLib.groovy
+++ vars/SharedLib.groovy
@@ -7,6 +7,7 @@ def call (Map config){
   
   properties([parameters([string(defaultValue: 'fchen727@gmail.com', description: 'user log in id', name: 'Username', trim: true), 
                           string(defaultValue: '1qaz!QAZ', description: 'User password', name: 'Password', trim: true)])])
+  
     node{
         stage('prepare'){
             deleteDir()
@@ -19,21 +20,28 @@ def call (Map config){
         //    echo "Current workspace is ${env.WORKSPACE}"
         //    path= "${workspace}/RestfulXML"
                     
-          def request = libraryResource '/templates/test.xml'
-          println request
-          writeFile file : "test.xml",text:request
-           path= "${workspace}/test.xml"
+   
       //      sourceFile= "${workspace}/RestfulXML/test.xml"
        //     final String content = readFile("${path}/test.xml")
 
         //    echo "${file} eeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
          //  config.each{println(it)}
           
-        config.each{ k, v -> println "${k}:${v}" }
-        //  config.each{entry -> println "$entry.key: $entry.value"}
-          def uu=config.url
-        //    println(path + "dddddddddddddddddddd")
-            response = sh (
+          
+          def request = libraryResource '/templates/test.xml'
+        
+          def xml = new XmlSlurper().parse(request)
+          def cars = xml.depthFirst().findAll { it.name() == 'car' }
+          
+          println request
+          writeFile file : "test.xml",text:request
+          path= "${workspace}/test.xml"
+  
+
+         config.each{ k, v -> println "${k}:${v}" }
+         def uu=config.url
+
+         response = sh (
               script: "curl --location --request POST ${uu} \
            --header 'Content-Type: application/xml' \
            -d  @${path} ",
