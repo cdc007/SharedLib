a1409b081a118fbde9f3cb6f3430b923f4ad73cd
cdc007 <dacheng_chen@yahoo.com>
2021-04-18 18:13:39 -0700

    Update SharedLib.groovy

--- vars/SharedLib.groovy
+++ vars/SharedLib.groovy
@@ -14,11 +14,15 @@ def call (Map config){
             println sh(script: 'pwd', returnStdout: true).trim()
             println sh(script: 'ls -lrt', returnStdout: true).trim()
 
-            workspace = env.WORKSPACE
-            sh "chmod 755 ${workspace}"
-            echo "Current workspace is ${env.WORKSPACE}"
+      //      workspace = env.WORKSPACE
+        //    sh "chmod 755 ${workspace}"
+        //    echo "Current workspace is ${env.WORKSPACE}"
         //    path= "${workspace}/RestfulXML"
-            sourceFile= "${workspace}/RestfulXML/test.xml"
+                    
+          def request = libraryResource '/templates/test.xml'
+          println request
+          writeFile file : "test.xml",text:request
+      //      sourceFile= "${workspace}/RestfulXML/test.xml"
        //     final String content = readFile("${path}/test.xml")
 
         //    echo "${file} eeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
@@ -31,16 +35,12 @@ def call (Map config){
             response = sh (
               script: "curl --location --request POST ${uu} \
            --header 'Content-Type: application/xml' \
-           -d  @${sourceFile} ",
+           -d  @${request} ",
                     returnStdout: true
             ).trim()
             println(response)
           
-          
-          def request = libraryResource '/templates/test.xml'
-          println request
-          
-          writeFile file : "test1.xml",text:request
+
         }
       
       
